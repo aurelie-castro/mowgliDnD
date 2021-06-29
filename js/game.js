d784aab9954484eea4053f33785525c44fe99ece
aurelie-castro <acdemarly@gmail.com>
2021-06-29 14:47:42 +0200

    zoned, changed link to kaa and changed title name

--- js/game.js
+++ js/game.js
@@ -41,14 +41,14 @@ function init() {
 }
 
 function preload() {
-    this.load.image('background', './assets/peterpanFull-01.png');
+    this.load.image('background', './assets/mowgli-01.png');
     
-    this.load.image('head', './assets/pHead-01.png');
-    this.load.image('body', './assets/pBody-01.png');
-    this.load.image('handL', './assets/pArmL-01.png');
-    this.load.image('handR', './assets/pArmR-01.png');
-    this.load.image('legL', './assets/pLegL-01.png');
-    this.load.image('legR', './assets/pLegR-01.png');
+    this.load.image('head', './assets/mowgliHead-01.png');
+    this.load.image('body', './assets/mowgliBody-01.png');
+    this.load.image('handL', './assets/mowgliHandL-01.png');
+//    this.load.image('handR', './assets/pArmR-01.png');
+    this.load.image('legL', './assets/mowgliLegL-01.png');
+    this.load.image('legR', './assets/mowgliLegR-01.png');
     
     this.load.image('nextArrow', './assets/green-arrow (1).png');
     
@@ -98,7 +98,7 @@ function create() {
     soundButton.on('pointerdown', enableMusic);
     
     //----les membres-----
-    var head = this.add.image(50, 380, 'head', Phaser.Math.RND.pick(frames)).setInteractive();
+    var head = this.add.image(82, 530, 'head', Phaser.Math.RND.pick(frames)).setInteractive();
     this.input.setDraggable(head);
 //    head.setScale(2);
     head.setName('head');
@@ -110,19 +110,19 @@ function create() {
     nextArrow.setScale(0.7);
     nextArrow.setVisible(false);
     
-    var body = this.add.image(60, 550, 'body', Phaser.Math.RND.pick(frames)).setInteractive();
+    var body = this.add.image(215, 540, 'body', Phaser.Math.RND.pick(frames)).setInteractive();
     this.input.setDraggable(body);
     body.setName('body');
 //    body.setScale(0.45);
     
-    var handL = this.add.image(310, 92, 'handL', Phaser.Math.RND.pick(frames)).setInteractive();
+    var handL = this.add.image(50, 360, 'handL', Phaser.Math.RND.pick(frames)).setInteractive();
     this.input.setDraggable(handL);
     handL.setName('handL');
 //    handL.setScale(0.45);
     
-    var handR = this.add.image(200, 552, 'handR', Phaser.Math.RND.pick(frames)).setInteractive();
-    this.input.setDraggable(handR);
-    handR.setName('handR');
+//    var handR = this.add.image(200, 552, 'handR', Phaser.Math.RND.pick(frames)).setInteractive();
+//    this.input.setDraggable(handR);
+//    handR.setName('handR');
 //    hips.setScale(0.45);
     
     var legL = this.add.image(50, 212, 'legL', Phaser.Math.RND.pick(frames)).setInteractive();
@@ -130,37 +130,49 @@ function create() {
     legL.setName('legL');
 //    legL.setScale(0.45);
     
-    var legR = this.add.image(310, 570, 'legR', Phaser.Math.RND.pick(frames)).setInteractive();
+    var legR = this.add.image(320, 552, 'legR', Phaser.Math.RND.pick(frames)).setInteractive();
     this.input.setDraggable(legR);
     legR.setName('legR');
 //    legR.setScale(0.45);
     
     //-----les drop zones----
     //  A drop zone
-    var zone = this.add.zone(200, 95, 115, 120).setRectangleDropZone(115, 120);
+    var zone = this.add.zone(220, 125, 115, 120).setRectangleDropZone(115, 120);
     zone.setName('head');
     
     //  A drop zone
-    var zone2 = this.add.zone(211, 227, 80, 137).setRectangleDropZone(80, 137);
+    var zone2 = this.add.zone(232, 268, 90, 137).setRectangleDropZone(90, 137);
     zone2.setName('body');
     
     //  A drop zone
-    var zone3 = this.add.zone(135, 221, 65, 130).setRectangleDropZone(65, 130);
+    var zone3 = this.add.zone(150, 175, 65, 130).setRectangleDropZone(65, 130);
     zone3.setName('handL');
     
     
     //  A drop zone
-    var zone4 = this.add.zone(252, 383, 90, 170).setRectangleDropZone(90, 170);
+    var zone4 = this.add.zone(260, 384, 90, 100).setRectangleDropZone(90, 100);
     zone4.setName('legR');
     
     //  A drop zone
-    var zone5 = this.add.zone(160, 385, 90, 170).setRectangleDropZone(90, 170);
+    var zone5 = this.add.zone(170, 386, 90, 100).setRectangleDropZone(90, 100);
     zone5.setName('legL');
     
     //  A drop zone
-    var zone6 = this.add.zone(270, 230, 40, 130).setRectangleDropZone(40, 130);
-    zone6.setName('handR');
+//    var zone6 = this.add.zone(270, 230, 40, 130).setRectangleDropZone(40, 130);
+//    zone6.setName('handR');
 
+//          var graphics = this.add.graphics();
+//    graphics.lineStyle(2, 0xffff00);
+//    graphics.strokeRect(zone.x - zone.input.hitArea.width / 2, zone.y - zone.input.hitArea.height / 2, zone.input.hitArea.width, zone.input.hitArea.height);
+//    
+//    graphics.strokeRect(zone2.x - zone2.input.hitArea.width / 2, zone2.y - zone2.input.hitArea.height / 2, zone2.input.hitArea.width, zone2.input.hitArea.height);
+//    
+//    graphics.strokeRect(zone3.x - zone3.input.hitArea.width / 2, zone3.y - zone3.input.hitArea.height / 2, zone3.input.hitArea.width, zone3.input.hitArea.height);
+//    
+//    graphics.strokeRect(zone4.x - zone4.input.hitArea.width / 2, zone4.y - zone4.input.hitArea.height / 2, zone4.input.hitArea.width, zone4.input.hitArea.height);
+//    
+//    graphics.strokeRect(zone5.x - zone5.input.hitArea.width / 2, zone5.y - zone5.input.hitArea.height / 2, zone5.input.hitArea.width, zone5.input.hitArea.height);
+    
  
     this.input.on('dragstart', function (pointer, gameObject) {
 
@@ -215,7 +227,7 @@ else{
             gameObject.y = gameObject.input.dragStartY;
         }
         
-      if(successfulDropoff === 6){
+      if(successfulDropoff === 5){
             console.log("well done!!!!");
             nextArrow.setVisible(true);
             nextArrow.setInteractive();
@@ -233,7 +245,7 @@ else{
 
 
 function update() {
-    if(successfulDropoff === 6){
+    if(successfulDropoff === 5){
          starScale += 0.001;
         star.setScale(starScale);
         if (starScale > 0.2){
@@ -246,7 +258,7 @@ function update() {
 }
 function onClick(){
 //    window.open("https://www.google.com", "_blank");
-    window.location.replace("http://www.w3schools.com");
+    window.location.replace("https://games.caramel.be/kaa/index.html");
 
 }
 function enableMusic(){
